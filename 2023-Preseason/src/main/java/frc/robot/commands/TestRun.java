3c3658bdb948714a1bbbc2bab2c96ddf8a9976b3
Joshua Elmore <boyscout574@outlook.com>
2022-10-21 01:21:57 -0600

    Got a Motor spining

--- 2023-Preseason/src/main/java/frc/robot/commands/TestRun.java
+++ 2023-Preseason/src/main/java/frc/robot/commands/TestRun.java
@@ -4,36 +4,48 @@
 
 package frc.robot.commands;
 
-import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.subsystems.TestMotor;
+
+import java.util.function.DoubleSupplier;
+
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
 /** An example command that uses an example subsystem. */
-public class ExampleCommand extends CommandBase {
-  @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
-  private final ExampleSubsystem m_subsystem;
+public class TestRun extends CommandBase {
+  @SuppressWarnings({ "PMD.UnusedPrivateField", "PMD.SingularField" })
+  private final TestMotor subsystem;
+  private final DoubleSupplier powerSupplier;
 
   /**
    * Creates a new ExampleCommand.
    *
    * @param subsystem The subsystem used by this command.
    */
-  public ExampleCommand(ExampleSubsystem subsystem) {
-    m_subsystem = subsystem;
+  public TestRun(TestMotor subsystem, DoubleSupplier powerSupplier) {
+    this.subsystem = subsystem;
+    this.powerSupplier = powerSupplier;
+
     // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(subsystem);
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    subsystem.setPower(0);
+
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+    subsystem.setPower(powerSupplier.getAsDouble());
+  }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+  }
 
   // Returns true when the command should end.
   @Override
--- 2023-Preseason/src/main/java/frc/robot/subsystems/TestMotor.java
+++ 2023-Preseason/src/main/java/frc/robot/subsystems/TestMotor.java
@@ -4,11 +4,26 @@
 
 package frc.robot.subsystems;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMax.IdleMode;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class ExampleSubsystem extends SubsystemBase {
+public class TestMotor extends SubsystemBase {
   /** Creates a new ExampleSubsystem. */
-  public ExampleSubsystem() {}
+
+  private CANSparkMax motor;
+  private static final int MAX_CURRENT = 5;
+
+  public TestMotor(int CANid) {
+    motor = new CANSparkMax(CANid, MotorType.kBrushless);
+    motor.setInverted(true);
+    motor.setIdleMode(IdleMode.kCoast);
+    motor.setSmartCurrentLimit(MAX_CURRENT);
+
+    motor.set(0);
+  }
 
   @Override
   public void periodic() {
@@ -18,5 +33,9 @@ public class ExampleSubsystem extends SubsystemBase {
   @Override
   public void simulationPeriodic() {
     // This method will be called once per scheduler run during simulation
+  }
+  public void setPower(double power)
+  {
+    motor.set(power);
   }
 }
